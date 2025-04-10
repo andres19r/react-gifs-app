37a92fa2aed3142894ae98d83f7e99683cb299a1
Andres Rivero <andres19rivero@gmail.com>
2025-04-10 17:19:48 -0400

    add initial categories state and render category list

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
   return (
     <>
+      {/* title */}
       <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Gifs list */}
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
+
+      {/* Gif item */}
     </>
   );
 };
