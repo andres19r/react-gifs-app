19eb7db464c8dc16ae878b5c5d4d9dbb257f2761
Andres Rivero <andres19rivero@gmail.com>
2025-04-16 13:59:17 -0400

    test: add tests for GifGrid component

---
+++ tests/components/GifGrid.test.jsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock("../../src/hooks/useFetchGifs");
+
+describe("GifGrid tests", () => {
+  const category = "One Punch";
+  test("it should show the loading initially", () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
+    render(<GifGrid category={category} />);
+    expect(screen.getByText("Loading ...")).toBeTruthy();
+    expect(screen.getByText(category)).toBeTruthy();
+  });
+
+  test("it should show items after loading imgs with useFetchGifs", () => {
+    const gifs = [
+      {
+        id: "ABC",
+        title: "Saitama",
+        url: "https://localhost/saitama.jpg",
+      },
+      {
+        id: "123",
+        title: "Goku",
+        url: "https://localhost/goku.jpg",
+      },
+    ];
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false,
+    });
+    render(<GifGrid category={category} />);
+
+    expect(screen.getAllByRole("img").length).toBe(2);
+  });
+});
